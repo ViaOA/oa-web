56dcbad642ec18e0a259b893a0f0273f5f0c2247
vvia <vvia@viaoa.com>
2026-09-10 13:34:35 -0500

    oaweb

---
+++ src/main/java/com/viaoa/web/html/oa/OAToolbarOptionsDelegate.java
@@ -0,0 +1,196 @@
+/*  Copyright 1999 Vince Via vvia@viaoa.com
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+package com.viaoa.web.html.oa;
+
+/**
+ * @author vvia
+ */
+public class OAToolbarOptionsDelegate {
+	
+	
+	
+	public static void config(
+			OAToolbarOptions tboTop, 
+			OAToolbarOptions tboBottom,
+			OAToolbarOptions.Type tboType
+		) 
+	{
+		if (tboType == null) return;
+		if (tboTop == null && tboBottom == null) return;
+		OAToolbarOptions to;
+		
+		if (tboType == tboType.List || tboType == tboType.ListWithEdit || tboType == tboType.ListForMaster) {
+			if (tboTop != null) {
+				if (tboBottom == null) {
+					// all on top
+					to = tboTop;
+					to.add(
+						to.Icon | to.Label | to.Goto |  
+						to.Find |
+						to.Refresh | to.PlusNav | to.PlusMove | to.Save | to.New |
+						to.Add | to.Insert | to.Wizard | to.Remove | to.Delete |
+						to.HubSearch | to.Report | to.Download | to.Custom
+					);
+				}
+				else {
+					// split
+					to = tboTop;
+					to.add(to.Icon | to.Label | to.Find);
+					
+					to = tboBottom;
+					to.add(
+						to.Goto | 
+						to.Refresh | to.PlusNav | to.PlusMove | to.Save | to.New |
+						to.Add | to.Insert | to.Wizard | to.Remove | to.Delete |
+						to.HubSearch | to.Report | to.Download |
+						to.Custom | to.HubCalc
+					);
+				}
+			}
+			else {
+				// all on bottom
+				to = tboBottom;
+				to.add(
+					to.Icon | to.Label | to.Goto |  
+					to.Find |
+					to.Refresh | to.PlusNav | to.PlusMove | to.Save | to.New |
+					to.Add | to.Insert | to.Wizard | to.Remove | to.Delete |
+					to.HubSearch | to.Report | to.Download |
+					to.Custom | to.HubCalc
+				);
+			}
+
+			if (tboType == tboType.ListWithEdit) {
+				to = tboTop;
+				if (to != null) to.remove(to.Icon | to.Label | to.PlusNav);
+				to = tboBottom;
+				if (to != null) to.remove(to.Icon | to.Label | to.PlusNav);
+			}
+			else if (tboType == tboType.ListForMaster) {
+				to = tboTop;
+				if (to != null) to.remove(to.Icon | to.Label | to.PlusNav);
+				to = tboBottom;
+				if (to != null) to.remove(to.Icon | to.Label | to.PlusNav);
+			}
+		}
+		else {
+			if (tboType == tboType.Edit) {
+				if (tboTop != null) {
+					if (tboBottom == null) {
+						// all on top
+						to = tboTop;
+						to.add(
+							to.Icon | to.Label |  
+							to.Refresh | to.Save | to.New |
+							to.Wizard | to.Delete |
+							to.Report | to.Custom
+						);
+					}
+					else {
+						// split
+						to = tboTop;
+						to.add(to.Icon | to.Label);
+						
+						to = tboBottom;
+						to.add(
+							to.Refresh | to.Save | to.New |
+							to.Wizard | to.Delete |
+							to.Report |
+							to.Custom
+						);
+					}
+				}
+				else {
+					// all on bottom
+					to = tboBottom;
+					to.add(
+						to.Icon | to.Label |  
+						to.Refresh | to.Save | to.New |
+						to.Wizard | to.Delete |
+						to.Report | to.Custom
+					);
+				}
+			}
+			else { // EditWithList
+				// none
+			}
+		}
+	}
+	
+	
+	public static void configOLD(
+		OAToolbarOptions listTop, 
+		OAToolbarOptions listBottom, 
+		OAToolbarOptions editTop, 
+		OAToolbarOptions editBottom, 
+		final boolean willHaveDetailPanel,
+		final boolean supportsFind
+	) {
+
+		OAToolbarOptions to;
+		if (listTop != null) {
+			if (listBottom == null) {
+				to = listTop;
+				to.add(
+					to.Icon | to.Label | to.Goto |  
+					// to.GoBack |
+					to.Refresh | to.PlusNav | to.PlusMove | to.Save | to.New |
+					to.Add | to.Insert | to.Wizard | to.Remove | to.Delete |
+					to.HubSearch | to.Report | to.Download | to.Custom
+				);
+				if (supportsFind) to.add(to.Find);
+			}
+			else {
+				to = listTop;
+				if (willHaveDetailPanel) {
+					if (supportsFind) to.add(to.Find);
+				}
+				else to.add(to.Icon | to.Label | to.Find);
+				
+				to = listBottom;
+				to.add(
+					to.Goto | to.Details |
+					// to.GoBack |
+					to.Refresh | to.PlusNav | to.PlusMove | to.Save | to.New |
+					to.Add | to.Insert | to.Wizard | to.Remove | to.Delete |
+					to.HubSearch | to.Report | to.Download |
+					to.Custom | to.HubCalc
+				);
+				if (supportsFind) to.add(to.Find);
+			}
+			return;
+		}
+		else if (listBottom != null) {
+			to = listBottom;
+			to.add(
+				to.Icon | to.Label | to.Goto |  
+				// to.GoBack |
+				to.Refresh | to.PlusNav | to.PlusMove | to.Save | to.New |
+				to.Add | to.Insert | to.Wizard | to.Remove | to.Delete |
+				to.HubSearch | to.Report | to.Download |
+				to.Custom | to.HubCalc
+			);
+			if (supportsFind) to.add(to.Find);
+			return;
+		}
+
+		if (editTop != null) {
+			to = editTop;
+			to.add(
+				to.Icon | to.Label | 
+				// to.GoBack |
+				to.Refresh | to.Save | to.New |
+				to.Wizard | to.Remove | to.Delete |
+				to.Report | to.Download | to.Custom
+			);
+		}
+	}
+}
